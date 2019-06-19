5070f1782670cfad2e212dd8ae04d39aa53d83ee
chenbin <cblovepp1314520>
2019-06-19 18:01:12 +0800

    项目创建

---
+++ cb-readwrite-demo/src/main/java/com/database/demo/cbreadwritedemo/CbReadwriteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.database.demo.cbreadwritedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbReadwriteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbReadwriteDemoApplication.class, args);
+    }
+
+}
